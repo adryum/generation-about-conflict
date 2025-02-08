45b9e92c2a4b9c6c3cbd86de6761cd5df852a1ad
adrians <adiskir@gmail.com>
2025-02-08 17:23:26 +0200

    scroll on hover

---
+++ generation-about-conflict/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+<!--  <header>-->
+
+<!--      <nav>-->
+<!--        <RouterLink to="/">Home</RouterLink>-->
+<!--        <RouterLink to="/about">About</RouterLink>-->
+<!--      </nav>-->
+
+<!--  </header>-->
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
